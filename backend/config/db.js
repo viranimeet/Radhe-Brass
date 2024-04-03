d7af3f5dfa6d89cf5cb2e38270e36fabeb18fb4f
viranimeet <viranimeetkumar@gmail.com>
2024-04-03 22:51:45 +0530

    6

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,37 +1,18 @@
-// import mongoose from 'mongoose';
-// import config from 'config';
-// require('dotenv').config();
-
-// const db = config.get('mongoURI');
-
-// const connectDB = async () => {
-//   try {
-//     mongoose.set('strictQuery', true);
-//     await mongoose.connect(db);
-//     console.log('MongoDB connected...');
-//   } catch (err) {
-//     console.error('Failed to connect to MongoDB:', err);
-//     process.exit(1);
-//   }
-// };
-
-
-// export { connectDB };
-
-
-
 import mongoose from 'mongoose';
-require('dotenv').config();
+import config from 'config';
+
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
     mongoose.set('strictQuery', true);
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(db);
     console.log('MongoDB connected...');
   } catch (err) {
     console.error('Failed to connect to MongoDB:', err);
     process.exit(1);
   }
 };
+
 
 export { connectDB };
